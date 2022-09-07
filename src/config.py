b0accd1b56a422313c9ef9fafc35ef383ff85df3
Lucas Sciuti <lfsciuti@gmail.com>
2022-09-07 09:13:43 -0300

    Adding config as python file

---
+++ src/config.py
@@ -0,0 +1,5 @@
+seed = 42
+dataset = {
+    "cols": ["score", "title", "review"],
+    "path": "./data/amazon_review_full_csv"
+}
--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,23 @@
 import pandas as pd
-
+from config import dataset
 
 def read_train_test():
     """
         Read train and test datasets and return then    
     """
-    
-    cols = ["score", "title", "review"]
 
-    train = pd.read_csv("../data/amazon_review_full_csv/train.csv", names=cols)
-    test = pd.read_csv("../data/amazon_review_full_csv/test.csv", names=cols)
-
+    train = pd.read_csv(
+        dataset["path"] + "/train.csv",
+        names=dataset["cols"]
+    )
+    print("Train dataset read")
+    test = pd.read_csv(
+        dataset["path"] + "/test.csv",
+        names=dataset["cols"]
+    )
+    print("Test dataset read")
     return train, test
+
+read_train_test()
 
 
